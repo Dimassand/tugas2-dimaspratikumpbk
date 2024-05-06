1c630249572032d7e1c7210afcb91813540a551f
Dimassand <Dimassand>
2024-05-06 22:39:58 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+
+import { ref } from 'vue'
+
+const text = ref('')
+</script>
+
+<template>
+  <input type="text" placeholder="Masukkan Hasil...">
+  <p>{{ text }}</p>
+</template>
